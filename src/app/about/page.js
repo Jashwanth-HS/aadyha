5a0cc5ca8d3691dfee454179f82d129cb22e3e16
Nihal K <nihal.a@supercode.in>
2024-03-22 18:19:44 +0530

    inital commit homepage earth moon section done

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+import Banner from './components/Banner';
+
+export default function About() {
+  return (
+    <div>
+        <Banner />
+        about
+        </div>
+  )
+}
---
+++ src/app/home/page.js
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+import Earth from './components/Earth';
+import Moon from './components/Moon';
+
+export default function Home() {
+  return (
+    <div>
+      <Earth />
+      <Moon />
+    </div>
+  )
+}
