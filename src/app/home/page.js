bd5ee22e6ada46c120d5881c8f7d2ae3cdd008fe
Nihal K <nihal.a@supercode.in>
2024-03-26 13:20:24 +0530

    home page space system section ui

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,12 +2,16 @@
 import React from 'react'
 import Earth from './components/Earth';
 import Moon from './components/Moon';
+import Mars from './components/Mars';
+import SpaceSystem from './components/SpaceSystem';
 
 export default function Home() {
   return (
     <div>
       <Earth />
       <Moon />
+      <Mars />
+      <SpaceSystem />
     </div>
   )
 }
