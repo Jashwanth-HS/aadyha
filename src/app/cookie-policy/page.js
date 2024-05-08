25d2269ed1ad15838288007f16c125fc26d8c899
Jashwanth-HS <jashwanth.hs@supercode.in>
2024-05-08 10:39:02 +0530

    removed unwanted files from project

--- src/app/cookie-policy/page.js
+++
@@ -1,6 +0,0 @@
-import ComingSoon from "@/components/ComingSoon";
-import React from "react";
-
-export default function CookiePolicyPage() {
-  return <ComingSoon page={"Cookie Policy"} />;
-}
--- src/app/privacy-policy/page.js
+++
@@ -1,6 +0,0 @@
-import ComingSoon from "@/components/ComingSoon";
-import React from "react";
-
-export default function PrivacyPolicyPage() {
-  return <ComingSoon page={"Privacy Policy"} />;
-}
